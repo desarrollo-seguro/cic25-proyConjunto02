a642e37fa63987037712e64abff1d213e1c61a78
HeDichoQueNo <no@mail.no>
2025-07-21 12:53:08 +0200

    Configuracion proyecto

---
+++ src/main/java/es/cic/curso25/cic25_proyConjunto02/Cic25ProyConjunto02Application.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.cic25_proyConjunto02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cic25ProyConjunto02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cic25ProyConjunto02Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/curso25/cic25_proyConjunto02/Cic25ProyConjunto02ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.cic25_proyConjunto02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cic25ProyConjunto02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
